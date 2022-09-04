62022b7ad84dcdd4dea2ee3c4feff56e5384ffb0
Leyvino Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2022-09-04 03:19:22 -0300

    next

--- Questão2(b).asm
+++ Questão2(b).asm
@@ -1,5 +1,15 @@
 .data
-
-
+	f: .word 0
+	g: .word 10 
+	A: .word 1, 2, 3, 4, 5 #vetor inicializado
+	B: .word 5, 4, 3, 2, 1       #vetor de 10 posisao inicializada
 
 .text
+
+	la $s7, B      #end. B
+	la $s6, A      #end. A
+	
+	lw $t0, 16($s7)     #Valor[4]
+        sll $t1, $t0, 2
+        
+        la
