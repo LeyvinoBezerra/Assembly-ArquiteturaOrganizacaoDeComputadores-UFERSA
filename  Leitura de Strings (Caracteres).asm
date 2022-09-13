f5400deb19b351082df89b774600e8beba92b9c4
Leyvino Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2022-09-13 12:36:39 -0300

    Create  Leitura de Strings (Caracteres).asm

---
+++  Leitura de Strings (Caracteres).asm
@@ -0,0 +1,28 @@
+.data
+	
+	pergunta: .asciiz "Qual o seu nome? "
+	saudacao: .asciiz "ola,  "
+	nome: .space 25
+	
+.text
+	#impressao da pergunta
+	li $v0, 4
+	la $a0, pergunta
+	syscall
+	
+	#leitura do nome
+	li $v0, 8
+	la $a0, nome
+	la $a1, 25
+	syscall
+	
+	#impressao da saudacao
+	li $v0, 4
+	la $a0, saudacao
+	syscall
+	
+	#impressao de nome
+	#impressao da pergunta
+	li $v0, 4
+	la $a0, nome
+	syscall
