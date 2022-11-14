fb2a7cd433fd3285fb9ebde2976435ca7b42627a
Leyvino Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2022-11-14 14:55:29 -0300

    Update Leitura de Strings (Caracteres).asm

--- Leitura de Strings (Caracteres).asm
+++ Leitura de Strings (Caracteres).asm
@@ -1,28 +1,26 @@
-.data
-	pergunta: .asciiz "Qual é o seu nome?"
-	saudacao: .asciiz "Olá,  " 
+.data 
+	pergunta: .asciiz "Qual é o seu nome? "
+	saudacao: .asciiz "ola, "
 	nome: .space 25
-	
+
 .text
-	#impressao da perguta
-	li $v0, 4
-	la $a0, pergunta
-	syscall 
-	
-	#leitura do nome
-	li $v0, 8
-	la $a0, nome
-	la $a1, 25
-	syscall 
-	
-	#mostra a saudação
-	li $v0, 4
-	la $a0, saudacao
-	syscall 
-	
-	#impressão do nome
-	li $v0, 4
-	la $a0, nome
-	syscall 
-	
-	
+         #impressao da pergunta
+         li $v0, 4
+         la $a0, pergunta
+         syscall
+         
+        #leitura do nome
+        li $v0, 8
+        la $a0, nome
+        la $a1, 25 
+        syscall 
+        
+        #mostra a saudacao
+        li $v0, 4
+        la, $a0, saudacao
+        syscall 
+        
+        #mostra a saudacao
+        li $v0, 4
+        la, $a0, nome
+        syscall 
