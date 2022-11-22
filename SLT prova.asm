c0c30769f2497ee21999d0dc7e20889f177f651a
Leyvino Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2022-11-22 09:10:08 -0300

    add

--- SLT prova.asm
+++ SLT prova.asm
@@ -1,3 +1,14 @@
+.data 
+ 	int: .word 20
 .text 
+	li $v0, 5
+	lw $a0, int
 
-slt $t0, $s0, $zero / beq $t0, $zero, fim1 / fim2: li $a0, 404 / li $v0, 17 / syscall / fim1: li $v0, 10 / syscall
+	move $t0, $a0
+	slt $t0, $s0, $zero 
+	beq  $t0, $zero, fim1
+	fim2: li $a0, 404  
+	li $v0, 17 
+ 	syscall
+ 	fim1: li $v0, 10 
+ 	syscall
